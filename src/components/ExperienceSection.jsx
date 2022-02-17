f3230084d11d97be5009d816f8baa1cf033ded8e
Alexander Lansdorp <alexlansdorprs@gmail.com>
2022-02-16 22:05:26 -0500

    map method

--- src/components/ExperienceSection.jsx
+++ src/components/ExperienceSection.jsx
@@ -2,31 +2,22 @@ import React from "react";
 import data from '../data.json';
 import ExperienceCard from "./ExperienceCard";
 
-export default class ExperieceSection extends React.Component {
-    render() {
-        return (
-            <React.Fragment>
-                <div id="experience-section" className="container-fluid">
-                    <div className="row">
+export default function ExperieceSection() {
+    const myExperiences = data.experience
+    return(
+        <React.Fragment>
+            <div id="experience-section" className="container-fluid">
+                <div className="row">
                     <p className="section-title">Experience</p>
-                        <ExperienceCard company={data.experience[0].company}
-                            startDate={data.experience[0].startDate}
-                            endDate={data.experience[0].endDate}
-                            jobTitle={data.experience[0].jobTitle}
-                            jobDescription={data.experience[0].jobDescription} />
-                        <ExperienceCard company={data.experience[1].company}
-                            startDate={data.experience[1].startDate}
-                            endDate={data.experience[1].endDate}
-                            jobTitle={data.experience[1].jobTitle}
-                            jobDescription={data.experience[1].jobDescription} />
-                        <ExperienceCard company={data.experience[2].company}
-                            startDate={data.experience[2].startDate}
-                            endDate={data.experience[2].endDate}
-                            jobTitle={data.experience[2].jobTitle}
-                            jobDescription={data.experience[2].jobDescription} />
-                    </div>
+                    {myExperiences.map((experience)=>
+                    <ExperienceCard company={experience.company}
+                        startDate={experience.startDate}
+                        endDate={experience.endDate}
+                        jobTitle={experience.jobTitle}
+                        jobDescription={experience.jobDescription} />
+                    )}
                 </div>
-            </React.Fragment>
-        )
-    }
+            </div>
+        </React.Fragment>
+    )
 }
--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -2,24 +2,21 @@ import React from "react";
 import data from '../data.json';
 import ProjectCard from "./ProjectCard";
 
-export default class ProjectsSection extends React.Component {
-    render() {
-        return (
-            <React.Fragment>
-                <div id="projects-section" className="container-fluid">
-                    <div className="row">
-                        <p className="section-title">Projects</p>
-                        <ProjectCard projectName={data.projects[0].projectName}
-                            url={data.projects[0].url} />
-                        <ProjectCard projectName={data.projects[1].projectName}
-                            url={data.projects[1].url} />
-                        <ProjectCard projectName={data.projects[2].projectName}
-                            url={data.projects[2].url} />
-                        <ProjectCard projectName={data.projects[3].projectName}
-                            url={data.projects[3].url} />
-                    </div>
+export default function ProjectsSection() {
+
+    const myProjects = data.projects
+    console.log(myProjects);
+    return (
+        <React.Fragment>
+            <div id="projects-section" className="container-fluid">
+                <div className="row">
+                    <p className="section-title">Projects</p>
+                    {myProjects.map((project) => 
+                        <ProjectCard projectName={project.projectName}
+                            url={project.url} />
+                    )}
                 </div>
-            </React.Fragment>
-        )
-    }
+            </div>
+        </React.Fragment>
+    )
 }
